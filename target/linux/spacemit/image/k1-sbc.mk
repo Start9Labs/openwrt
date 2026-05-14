e0561176001ca3723422d985271a9d41941b67a5
Shadowy Super Coder <musashidisciple@proton.me>
2026-05-13 19:37:28 -0600

    spacemit: build sysupgrade image for BPI-F3
    Add a sysupgrade.img.gz output for bananapi-f3 so OTA updates can
    flash via sysupgrade. Rework sdcard-img to rename the generated
    image to the expected build output path instead of copying it to
    BIN_DIR, which lets downstream image steps (gzip, append-metadata)
    chain off it.

--- target/linux/spacemit/image/k1-sbc.mk
+++ target/linux/spacemit/image/k1-sbc.mk
@@ -12,8 +12,9 @@ define Device/bananapi-f3
   KERNEL_NAME := Image
   KERNEL_IMG := Image.itb
   KERNEL := kernel-bin | fit none $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb
-  IMAGES := pack
+  IMAGES := pack sysupgrade.img.gz
   IMAGE/pack := boot-common | sdcard-img
+  IMAGE/sysupgrade.img.gz := boot-common | sdcard-img | gzip | append-metadata
 endef
 TARGET_DEVICES += bananapi-f3
 
